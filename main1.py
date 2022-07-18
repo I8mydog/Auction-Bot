4aeddf037ea9c6e92f26601dbcbe56a5c1cb7d82
Marcus Dedrick <41016297+I8mydog@users.noreply.github.com>
2022-07-17 22:49:42 -0400

    Update main1.py
    Added lb and biddel command, working on having the bot send a message when the auction ends

--- main1.py
+++ main1.py
@@ -1,19 +1,24 @@
 from calendar import day_abbr, month
 from datetime import datetime
 from datetime import timedelta
-import json
+import shutil
 import os
 import time
 import csv
-from turtle import end_fill
+from turtle import end_fill, title
 from unicodedata import name
 import discord
 import asyncio
 from discord.ext import commands
 from setuptools import Command
-
+from random import randint
+import threading
+import logging
 
 bot = commands.Bot(command_prefix='!')
+
+def modCommands(ctx):
+    return ctx.role.id == 915071048779726849
 
 @bot.command()
 async def abt(ctx):
@@ -52,13 +57,8 @@ async def abt(ctx):
                 "inline": False                
             },
             {
-                "name": '!leaderboard',
+                "name": '!lb',
                 "value": 'Shows the bidding leaderboard for the current auction',
-                "inline": False                
-            },
-            {
-                "name": '!remove',
-                "value": 'Removes an auction',
                 "inline": False                
             }
         ]
@@ -68,25 +68,16 @@ async def abt(ctx):
         embed = discord.Embed.from_dict(embed_data)
     )
     
-@bot.command()
-async def info(ctx):
-
-    await ctx.send(ctx.guild)
-    await ctx.send(ctx.author)
-    await ctx.send(ctx.message.id)
-
-
-
 
 global CreationTime, EndingTime, ItemNameGlobal, StartingBidGlobal
-
+EndingTime = 0
 
 
 @bot.command()
 async def create(ctx, ItemName, AuctionDuration, StartingBid):
     global CreationTime, EndingTime, ItemNameGlobal, StartingBidGlobal
     CreationTime = int(time.time())
-    EndingTime = int(time.time()) + int(AuctionDuration) * 86400
+    EndingTime = int(time.time()) + int(AuctionDuration)
     ItemNameGlobal = ItemName
     StartingBidGlobal = StartingBid
     print(CreationTime)
@@ -154,8 +145,9 @@ async def create(ctx, ItemName, AuctionDuration, StartingBid):
 
 
 @bot.command()
-
 async def auctioninfo(ctx):
+    with open('biddata.csv', 'r', newline='\n') as csvfile:
+        bids = list(csv.reader(csvfile, delimiter=','))
     embed1 = discord.Embed(
         title='Current Auction'
     )
@@ -171,7 +163,7 @@ async def auctioninfo(ctx):
     )
     embed1.add_field(
         name='Current Bid',
-        value='Placeholder',
+        value=f'{bids[-1][1]} nugs by <@{bids[-1][0]}>',
         inline=True
     )
     embed1.add_field(
@@ -187,7 +179,6 @@ async def auctioninfo(ctx):
 @bot.command()
 async def bid(ctx, bidAmount):
     global EndingTime, StartingBidGlobal
-    print(EndingTime)
     """EndingTime = int(time.time()) + 10"""
     if EndingTime < int(time.time()):
         await ctx.send('There is currently no auction running. Please check with I8mydog or Boone for upcoming auctions.')
@@ -210,14 +201,83 @@ async def bid(ctx, bidAmount):
         await ctx.send(f'Bid successful! The current highest bid is {bidAmount}.')
     else:
         await ctx.send(f'Bid must be at least 5 nugs greater than {lastbid} nugs.')
+    
+@bot.command()
+async def biddel(ctx):
+    with open('biddata.csv', 'r', newline='\n') as csvfile:
+        bids = list(csv.reader(csvfile, delimiter=','))
+    for row in bids[::-1]:
+        if int(row[0]) == ctx.author.id:
+            bids.remove(row)
+            await ctx.send('Bid Deleted')            
+            with open('biddata.csv', 'w', newline='\n') as csvfile:
+                bidlist = csv.writer(csvfile, delimiter=',')
+                for row in bids:
+                    bidlist.writerow(row)
+            return
+    await ctx.send('Bid not found')
+
+@bot.command()
+async def lb(ctx):
+    with open('biddata.csv', 'r', newline='\n') as csvfile:
+        bids = list(csv.reader(csvfile, delimiter=','))
+        lbarray = [str(944677979185872988)]
+        lbembed = discord.Embed(
+            title= "Leaderboard"
+        )
+        for row in bids[::-1]:
+            if row[0] in lbarray:
+                continue
+            lbembed.add_field(
+                name= f'{len(lbarray)}. {row[1]} nuggets',
+                value= f'<@{row[0]}>',
+                inline = False
+                )
+            lbarray.append(row[0])
+        
+    await ctx.send(embed = lbembed)
+
+@bot.command()
+async def bidhist(ctx, userid):
+    with open('biddata.csv', 'r', newline='\n') as csvfile:
+        bids = list(csv.reader(csvfile, delimiter=','))
+        bidhistfield = ''
+        bidhistarray = []
+        for row in bids[::-1]:
+            if userid == row[0]:
+                bidhistfield += f'{len(bidhistarray)+1}. {row[1]} nuggets\n'
+                bidhistarray.append(row[1])
+        
+        bidhistembed = discord.Embed(
+            title= "Bid History"    
+        )
+        bidhistembed.add_field(
+            name= bidhistfield,
+            value= f'<@{userid}>'
+        )
+
+    await ctx.send(embed = bidhistembed)
+
+async def check(ctx, thread1):
+    logging.info("Thread %s: starting", thread1)
+    logging.info("Thread %s: finishing", thread1)
+    while time.time() < EndingTime:
+        time.sleep(0.5)
+    if EndingTime < time.time():
+        with open('biddata.csv', 'r', newline='\n') as csvfile:
+            bids = list(csv.reader(csvfile, delimiter=','))
+        endofauction = discord.Embed(
+            title= "Auction Ended",    
+        )
+        endofauction.addfield(
+            name= "The winner of the auction is",
+            value= f'{bids[-1][0]} with {bids[-1][1]} nuggets!'
+        )
+    await ctx.send(embed = endofauction)
 
 
 
- 
-
-
-
-
+    
 
 
 
